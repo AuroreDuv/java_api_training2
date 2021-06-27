75b0e4d601a77440a2752b2d9b4ef9aff0079e4b
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-06-28 00:28:21 +0200

    Part 3 - Create start and stop server methods

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -2,19 +2,34 @@ package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Launcher {
-    public static void main(String[] args) throws Exception {
-        int port = Integer.parseInt(args[0]);
+    private HttpServer server;
 
+    public void start_server(int port) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         ExecutorService executor = Executors.newFixedThreadPool(1);
         server.createContext("/ping", new CallGetHandler());
         server.createContext("/api/game/start", new CallPostHandler());
         server.setExecutor(executor);
         server.start();
+
+        this.server = server;
+    }
+
+    public void stop_server() {
+        this.server.stop(0);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+
+        Launcher launcher = new Launcher();
+
+        launcher.start_server(port);
     }
 }
