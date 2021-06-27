4f210ab3b682a861309cea39cb64c2b058c5140a
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-06-27 16:14:05 +0200

    Part 3 - LauncherTest check that server is started

--- src/test/java/fr/lernejo/navy_battle/LauncherTest.java
+++ src/test/java/fr/lernejo/navy_battle/LauncherTest.java
@@ -1,6 +1,10 @@
 package fr.lernejo.navy_battle;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.Socket;
 
 class LauncherTest {
     @Test
@@ -8,5 +12,10 @@ class LauncherTest {
         String[] args = {"9876"};
         Launcher launcher = new Launcher();
         launcher.main(args);
+
+        try (Socket s = new Socket("localhost", 9876)) {
+        } catch (IOException ex) {
+            Assertions.fail("Server not started");
+        }
     }
 }
