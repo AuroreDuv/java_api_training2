2fc54a0b321946acca908e05af9e3e1c47b4374a
Aurore DUVERNOY <aduvernoy@visualstorm.fr>
2021-06-30 20:09:41 +0200

    Test fire call after start

--- src/main/java/fr/lernejo/navy_battle/StartGame.java
+++ src/main/java/fr/lernejo/navy_battle/StartGame.java
@@ -15,10 +15,14 @@ class StartGame implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String body;
         try (InputStream inputSchema = getClass().getResourceAsStream("/schema.json")) { // Get Json Schema for validation
-            SchemaLoader.load(new JSONObject(new JSONTokener(inputSchema))).validate(new JSONObject(new JSONTokener(exchange.getRequestBody()))); // Json Schema Validation
+            JSONObject request = new JSONObject(new JSONTokener(exchange.getRequestBody()));
+            SchemaLoader.load(new JSONObject(new JSONTokener(inputSchema))).validate(request); // Json Schema Validation
             // Send response with status Accepted 202
             body = "{\"id\": \"2aca7611-0ae4-49f3-bf63-75bef4769028\", \"url\": \"http://" + exchange.getRequestHeaders().getFirst("Host") + "\", \"message\": \"May the best code win\"}";
             exchange.sendResponseHeaders(202, body.length());
+
+            ProcessBuilder pb = new ProcessBuilder("curl", request.get("url") + "/api/game/fire?cell=B2");
+            Process p = pb.start();
         } catch (Exception e) {
             body = "Bad Request";
             exchange.sendResponseHeaders(400, body.length());
